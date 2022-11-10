7019b48414c5edaf0e56f3d8d540d7ae48c3a65e
lbroglio <lbroglio@iastate.edu>
2022-11-09 19:23:41 -0600

    Started  individual opeCode Parsers and added an underscore to the end opeCodeMap keys

--- i281Assembler.cpp
+++ i281Assembler.cpp
@@ -10,32 +10,32 @@
 //Maps Ope Codes to there 4 bit machine code identifier. 
 const std::unordered_map<std::string,std::string> opeCodeMap = 
 {
-    {"NOOP","0000"},
-    {"INPUTC","0001"},
-    {"INPUTCF","001"},
-    {"INPUTD","0001"},
-    {"INPUTDF","0001"},
-    {"MOVE","0010"},
-    {"LOADI","0011"},
-    {"LOADP","0011"},
-    {"ADD","0100"},
-    {"ADDI","0101"},
-    {"SUB","0110"},
-    {"SUBI","0111"},
-    {"LOAD","1000"},
-    {"LOADF","1001"},
-    {"STORE","1010"},
-    {"STOREF","1011"},
-    {"SHIFTL","1100"},
-    {"SHIFTR","1100"},
-    {"CMP","1101"},
-    {"JUMP","1110"},
-    {"BRE","1111"},
-    {"BRZ","1111"},
-    {"BRNE","1111"},
-    {"BRNZ","1111"},
-    {"BRG","1111"},
-    {"BRGE","1111"},
+    {"NOOP","0000_"},
+    {"INPUTC","0001_"},
+    {"INPUTCF","001_"},
+    {"INPUTD","0001_"},
+    {"INPUTDF","0001_"},
+    {"MOVE","0010_"},
+    {"LOADI","0011_"},
+    {"LOADP","0011_"},
+    {"ADD","0100_"},
+    {"ADDI","0101_"},
+    {"SUB","0110_"},
+    {"SUBI","0111_"},
+    {"LOAD","1000_"},
+    {"LOADF","1001_"},
+    {"STORE","1010_"},
+    {"STOREF","1011_"},
+    {"SHIFTL","1100_"},
+    {"SHIFTR","1100_"},
+    {"CMP","1101_"},
+    {"JUMP","1110_"},
+    {"BRE","1111_"},
+    {"BRZ","1111_"},
+    {"BRNE","1111_"},
+    {"BRNZ","1111_"},
+    {"BRG","1111_"},
+    {"BRGE","1111_"},
 };
 //Stores the addresses(Line number) of points the program can jump to 
 std::unordered_map<std::string,int> jumpAddressesMap;
@@ -43,6 +43,8 @@ std::unordered_map<std::string,int> jumpAddressesMap;
 partedCode asmCode;
 //Stores the current line of the program the assembler is operating on
 int currLineNum;
+//Stores the generated machine code as a string
+std::string machineCode = "";
 
 
 /**
@@ -70,13 +72,17 @@ void setJumpAddreses(){
     free(cursor);
 }
 
-
-
+void parseNOPE(){
+    std::string generatedMachineCodeLine = opeCodeMap.at("NOOP");
+    generatedMachineCodeLine += "00000000\n";
+    machineCode += generatedMachineCodeLine;
+}
 
 
 int main(){
     asmCode = parseCode("TestProgram.txt");
-
+    parseNOPE();
+    std::cout <<  machineCode;
 
 
 }
